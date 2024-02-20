b1ae3edee15420b91f639fa6275f1e6126d24811
Daniel Gilbers <danielgilbers@gmail.com>
2024-02-20 12:15:14 +0100

    set map location

--- js/map.js
+++ js/map.js
@@ -1,9 +1,10 @@
+/*
 const map = L.map('map').setView([50.827, 6.891], 15)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 20,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
-
+*/
 /**
  * Button definieren
  */
